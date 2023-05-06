3cf998f75cad72d0ba47f69b4485a026f129edb2
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-05-06 14:59:07 -0300

    Revert "pruba push"
    This reverts commit da525158438dca2b56fe323258f080d736868472.

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const Santiago = require('express').Router();
-const getHandler = require('./GetHandler');
-const postHandler = require('./PostHandler');
-
-Santiago.get('/', getHandler)
-Santiago.post('/', postHandler)
-
-module.exports = Santiago;
