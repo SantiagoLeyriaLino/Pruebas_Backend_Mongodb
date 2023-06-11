fbfe533b2e4dd8750fdcca8df5cecd0baedb3643
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-11 15:21:51 -0300

    vercel

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose')
 
-const DB_URI = 'mongodb://127.0.0.1/instituciones'
+const DB_URI = "mongodb://mongo:RBm7aDS8gHh5Qd9mERUm@containers-us-west-149.railway.app:6048"
 //mongodb://localhost:27017
 module.exports=()=>{
     const connect =()=>{
